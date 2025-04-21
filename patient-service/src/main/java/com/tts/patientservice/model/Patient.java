a076e1f1453651e5c274f32bb08d1c5815274444
toriqultonu <toriqul@mymedicalhub.com>
2025-04-21 20:23:13 +0600

    Add Patient entity and H2 database dependency
    Introduced the Patient entity with attributes such as name, email, address, date of birth, and registration date. Updated the `pom.xml` to include the H2 database dependency for testing purposes. This enables persistence and foundational setup for handling patient data.

---
+++ patient-service/src/main/java/com/tts/patientservice/model/Patient.java
@@ -0,0 +1,29 @@
+package com.tts.patientservice.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @NotNull
+    private String name;
+    @NotNull
+    @Email
+    @Column(unique = true)
+    private String email;
+    @NotNull
+    private String address;
+    @NotNull
+    private LocalDate dateOfBirth;
+    @NotNull
+    private LocalDate registeredDate;
+}
