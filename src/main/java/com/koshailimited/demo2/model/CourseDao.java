3747ff07c7ae473bc812214fef42b38ca3fe773e
toriqultonu <toriqul@mymedicalhub.com>
2025-04-21 19:17:51 +0600

    newly started

--- src/main/java/com/koshailimited/demo2/model/CourseDao.java
+++
@@ -1,8 +0,0 @@
-package com.koshailimited.demo2.model;
-
-import com.koshailimited.demo2.entities.Courses;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseDao extends JpaRepository<Courses, Long> {
-
-}
