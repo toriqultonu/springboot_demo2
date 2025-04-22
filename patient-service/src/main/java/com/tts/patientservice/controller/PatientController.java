10a8388a31a23c5699ef21da0cfce34bbefb70cc
toriqultonu <toriqul@mymedicalhub.com>
2025-04-22 13:18:52 +0600

    Add PatientController and DTOs for patient API
    Introduced `PatientController` to handle patient-related requests with an endpoint to fetch all patients. Added `PatientRequestDTO` for data validation and a sample HTTP request file for testing the patient API. These changes set the foundation for managing patient data effectively.

---
+++ patient-service/src/main/java/com/tts/patientservice/controller/PatientController.java
@@ -0,0 +1,27 @@
+package com.tts.patientservice.controller;
+
+import com.tts.patientservice.dto.PatientResponseDTO;
+import com.tts.patientservice.service.PatientService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/patients")
+public class PatientController {
+
+    private final PatientService patientService;
+
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PatientResponseDTO>> getPatients(){
+        List<PatientResponseDTO> patients = patientService.getPatients();
+        return ResponseEntity.ok().body(patients);
+    }
+}
---
+++ patient-service/src/main/java/com/tts/patientservice/dto/PatientRequestDTO.java
@@ -0,0 +1,65 @@
+package com.tts.patientservice.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class PatientRequestDTO {
+
+    @NotBlank(message = "Name is mandatory")
+    @Size(max = 100, message = "Name cannot exceed 100 characters")
+    private String name;
+
+    @NotBlank(message = "Email is mandatory")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotBlank(message = "Address is mandatory")
+    private String address;
+
+    @NotBlank(message = "Date of Birth is mandatory")
+    private String dateOfBirth;
+
+    @NotBlank(message = "Registered Date is mandatory")
+    private String registeredDate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRegisteredDate() {
+        return registeredDate;
+    }
+
+    public void setRegisteredDate(String registeredDate) {
+        this.registeredDate = registeredDate;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
