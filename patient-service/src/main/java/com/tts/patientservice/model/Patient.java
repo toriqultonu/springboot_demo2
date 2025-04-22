94a2a9160c81f6611e8594d08b98c103bbe88409
toriqultonu <toriqultonu22@gmail.com>
2025-04-22 10:45:21 +0600

    Add getters and setters to Patient model
    Implemented standard getter and setter methods for all fields in the `Patient` class to enable better encapsulation and data access. Additionally, removed the test scope from the H2 database dependency in the pom.xml for broader usage.

--- patient-service/src/main/java/com/tts/patientservice/model/Patient.java
+++ patient-service/src/main/java/com/tts/patientservice/model/Patient.java
@@ -26,4 +26,52 @@ public class Patient {
     private LocalDate dateOfBirth;
     @NotNull
     private LocalDate registeredDate;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public LocalDate getRegisteredDate() {
+        return registeredDate;
+    }
+
+    public void setRegisteredDate(LocalDate registeredDate) {
+        this.registeredDate = registeredDate;
+    }
 }
