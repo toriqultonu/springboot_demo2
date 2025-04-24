d1c849e348472cef879b12b6faec960a6d327b71
toriqultonu <toriqultonu22@gmail.com>
2025-04-24 09:43:22 +0600

    Add OpenAPI annotations and Springdoc dependency
    Integrated OpenAPI annotations into PatientController for API documentation, including operation summaries and a class-level tag. Added Springdoc dependency to `pom.xml` to enable OpenAPI support and Swagger UI integration.

--- patient-service/src/main/java/com/tts/patientservice/controller/PatientController.java
+++ patient-service/src/main/java/com/tts/patientservice/controller/PatientController.java
@@ -4,6 +4,8 @@ import com.tts.patientservice.dto.PatientRequestDTO;
 import com.tts.patientservice.dto.PatientResponseDTO;
 import com.tts.patientservice.dto.validators.CreatePatientValidationGroup;
 import com.tts.patientservice.service.PatientService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.groups.Default;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/patients")
+@Tag(name = "Patient", description = "Patient Service REST API")
 public class PatientController {
 
     private final PatientService patientService;
@@ -24,12 +27,14 @@ public class PatientController {
     }
 
     @GetMapping
+    @Operation(summary = "Get all patients")
     public ResponseEntity<List<PatientResponseDTO>> getPatients(){
         List<PatientResponseDTO> patients = patientService.getPatients();
         return ResponseEntity.ok().body(patients);
     }
 
     @PostMapping
+    @Operation(summary = "Create a new patient")
     public ResponseEntity<PatientResponseDTO> createPatient(@Validated({Default.class, CreatePatientValidationGroup.class})
                                                                 @RequestBody PatientRequestDTO patientRequestDTO){
         PatientResponseDTO patientResponseDTO = patientService.createPatient(patientRequestDTO);
@@ -37,12 +42,14 @@ public class PatientController {
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Update an existing patient")
     public ResponseEntity<PatientResponseDTO> updatePatient(@PathVariable UUID id, @Validated({Default.class}) @RequestBody PatientRequestDTO patientRequestDTO){
         PatientResponseDTO patientResponseDTO = patientService.updatePatient(id, patientRequestDTO);
         return ResponseEntity.ok().body(patientResponseDTO);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete an existing patient")
     public ResponseEntity<Void> deletePatient(@PathVariable UUID id){
         patientService.deletePatient(id);
         return ResponseEntity.noContent().build();
