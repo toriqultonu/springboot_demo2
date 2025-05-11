331a785aa9ad83b0d5d433e7fb0a19b2fa4234ee
toriqultonu <toriqultonu22@gmail.com>
2025-05-11 21:23:03 +0600

    Add VPC, subnets, databases, and MSK cluster to LocalStack
    This commit integrates a VPC with public and private subnets, RDS databases (AuthServiceDB and PatientServiceDB), and an MSK cluster into the LocalStack configuration. Health checks for both databases and resource configurations necessary for private networking are also added to scale infrastructure.

--- infrastructure/src/main/java/com/tts/stack/LocalStack.java
+++ infrastructure/src/main/java/com/tts/stack/LocalStack.java
@@ -1,12 +1,77 @@
 package com.tts.stack;
 
+
+
 import software.amazon.awscdk.*;
+import software.amazon.awscdk.services.ec2.*;
+import software.amazon.awscdk.services.ec2.InstanceType;
+import software.amazon.awscdk.services.msk.CfnCluster;
+import software.amazon.awscdk.services.rds.*;
+import software.amazon.awscdk.services.route53.CfnHealthCheck;
+
 import java.io.IOException;
+import java.util.stream.Collectors;
 
 public class LocalStack extends Stack {
 
+    private final Vpc vpc;
+
     public LocalStack(final App scope, final String id, final StackProps props){
          super(scope, id, props);
+
+         this.vpc = createVpc();
+
+         DatabaseInstance authServiceDb = createDatabase("AuthServiceDB", "auth-service-db");
+
+         DatabaseInstance patientServiceDb = createDatabase("PatientServiceDB", "patient-service-db");
+
+         CfnHealthCheck authDbHealthCheck = createDbHealthCheck(authServiceDb, "AuthServiceDBHealthCheck");
+
+         CfnHealthCheck patientDbHealthCheck = createDbHealthCheck(patientServiceDb, "PatientServiceDBHealthCheck");
+
+         CfnCluster mskCluster = createMskCluster();
+    }
+
+    private Vpc createVpc(){
+        return Vpc.Builder.create(this, "PatientManagementVPC").vpcName("PatientManagementVPC")
+                .maxAzs(2).build();
+    }
+
+    private DatabaseInstance createDatabase(String id, String dbName){
+        return DatabaseInstance.Builder.create(this, id).engine(DatabaseInstanceEngine.postgres(PostgresInstanceEngineProps
+                .builder().version(PostgresEngineVersion.VER_17_2).build()))
+                .vpc(vpc)
+                .instanceType(InstanceType.of(InstanceClass.BURSTABLE2, InstanceSize.MICRO))
+                .allocatedStorage(20)
+                .credentials(Credentials.fromGeneratedSecret("dexton"))
+                .databaseName(dbName)
+                .removalPolicy(RemovalPolicy.DESTROY)
+                .build();
+    }
+
+    private CfnHealthCheck createDbHealthCheck(DatabaseInstance db, String id){
+        return CfnHealthCheck.Builder.create(this, id)
+                .healthCheckConfig(CfnHealthCheck.HealthCheckConfigProperty.builder()
+                        .type("TCP").port(Token.asNumber(db.getDbInstanceEndpointPort()))
+                        .ipAddress(db.getDbInstanceEndpointAddress())
+                        .requestInterval(30)
+                        .failureThreshold(3)
+                        .build())
+                .build();
+    }
+
+    private CfnCluster createMskCluster(){
+        return CfnCluster.Builder.create(this, "MskCluster")
+                .clusterName("kafka-cluster")
+                .kafkaVersion("2.8.0")
+                .numberOfBrokerNodes(1)
+                .brokerNodeGroupInfo(CfnCluster.BrokerNodeGroupInfoProperty.builder()
+                        .instanceType("kafka.m5.large")
+                        .clientSubnets(vpc.getPrivateSubnets().stream().map(
+                                ISubnet::getSubnetId)
+                                .collect(Collectors.toList()))
+                        .brokerAzDistribution("DEFULT").build())
+                .build();
     }
 
     public static void main(final String[] args){
