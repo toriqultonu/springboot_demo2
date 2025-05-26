3eb5f2761d6b2643b271c977cf4783262aac6bc9
toriqultonu <toriqul@mymedicalhub.com>
2025-05-26 20:48:53 +0600

    Add test class with student sorting example.
    Introduced a `test` class that sorts a list of students by GPA in descending order and then by name. This serves as an example implementation of sorting logic, demonstrating comparator usage in Java.

--- patient-service/src/main/java/com/tts/patientservice/test.java
+++ patient-service/src/main/java/com/tts/patientservice/test.java
@@ -4,6 +4,7 @@ package com.tts.patientservice;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Vector;
 
 class Student {
     private String name;
@@ -27,16 +28,26 @@ public class test {
 
     public static void main(String[] args) {
 
-        List<Student> students = new ArrayList<>();
-        students.add(new Student("Alice", 3.5));
-        students.add(new Student("Bob", 3.8));
-        students.add(new Student("Charlie", 3.5));
-        students.add(new Student("David", 3.9));
+//        List<Student> students = new ArrayList<>();
+//        students.add(new Student("Alice", 3.5));
+//        students.add(new Student("Bob", 3.8));
+//        students.add(new Student("Charlie", 3.5));
+//        students.add(new Student("David", 3.9));
+//
+//        students.sort(Comparator.comparing(Student::getGpa).reversed().thenComparing(Student::getName));
+//        System.out.println("Students sorted by name:");
+//        for (Student student : students) {
+//            System.out.println(student.getName() + " - GPA: " + student.getGpa());
+//        }
 
-        students.sort(Comparator.comparing(Student::getGpa).reversed().thenComparing(Student::getName));
-        System.out.println("Students sorted by name:");
-        for (Student student : students) {
-            System.out.println(student.getName() + " - GPA: " + student.getGpa());
-        }
+        Vector<String> v = new Vector<>(3,100);
+        System.out.println(v.capacity());
+        v.add("A");
+        v.add("B");
+        v.add("C");
+        v.add("D");
+        v.add("D");
+        System.out.println(v.capacity());
+
     }
 }
